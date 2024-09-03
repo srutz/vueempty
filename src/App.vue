94f134749a1ab458d4be5904d268eb63bdd6732b
stepan.rutz <stepan.rutz@gmx.de>
2024-09-03 10:36:06 +0200

    loading and printing to console

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,25 @@
 <script setup lang="ts">
+import {onMounted} from "vue"
+
+type Quote = {
+  id: number
+  quote: string
+  author: string
+}
+type QuoteResponse = {
+  quotes: Quote[]
+  total: number
+  skip: number
+  limit: number
+}
+
+onMounted(async () => {
+  const res = await fetch("https://dummyjson.com/quotes?limit=5")
+  const json = await res.json()
+  const quotesResponse = json as QuoteResponse
+  console.table(quotesResponse.quotes)
+})
+
 </script>
 
 <template>
