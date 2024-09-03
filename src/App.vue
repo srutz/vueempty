c2e7afe02c6c7bd1a891bb97e7d2ab17297be5b5
stepan.rutz <stepan.rutz@gmx.de>
2024-09-03 12:51:37 +0200

    loading quotes using watchEffect

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import {onMounted, ref} from "vue"
+import {onMounted, ref, watchEffect} from "vue"
 import {QuoteType} from "./ApiTypes.ts";
 import Quote from "./Quote.vue"
 
@@ -9,21 +9,44 @@ type QuoteResponse = {
   skip: number
   limit: number
 }
-
+const CHUNK_SIZE = 2
 const quotes = ref<QuoteType[]>([])
+const offset = ref(0)
+const loading = ref(false)
 
-onMounted(async () => {
-  const res = await fetch("https://dummyjson.com/quotes?limit=15")
-  const json = await res.json()
-  const quotesResponse = json as QuoteResponse
-  console.table(quotesResponse.quotes)
-  quotes.value = quotesResponse.quotes
+const delay = async (delayMs: number) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, delayMs)
+  })
+}
+
+watchEffect(async () => {
+  loading.value = true
+  try {
+    console.time("timer1")
+    const res = await fetch("https://dummyjson.com/quotes"
+        + "?limit=" + encodeURIComponent(CHUNK_SIZE)
+        + "&skip=" + encodeURIComponent(offset.value))
+    const json = await res.json()
+    //await delay(10_000)
+    const quotesResponse = json as QuoteResponse
+    quotes.value.push(...quotesResponse.quotes)
+    console.timeEnd("timer1")
+  } finally {
+    loading.value = false
+  }
 })
+const loadmore = () => {
+  offset.value += CHUNK_SIZE
+}
+
 </script>
 <template>
   <div class="flex flex-col gap-2">
-    <div v-for="quote in quotes" :key="quote.id">
+    <div v-if="loading">Loading data</div>
+    <div v-else v-for="quote in quotes" :key="quote.id">
       <Quote :subject="quote" />
     </div>
+    <button @click="loadmore()">Load more quotes</button>
   </div>
 </template>
