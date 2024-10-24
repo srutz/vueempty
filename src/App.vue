9a75c61cf7236182c0c5a949182347266bc90e3c
stepan.rutz <stepan.rutz@gmx.de>
2024-10-24 15:38:09 +0200

    simple manual loading indicator

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,27 @@
-<script setup lang="ts">
-</script>
-
 <template>
-  <div class="text-3xl">hi</div>
+    <div class="bg-gray-200 h-screen flex flex-col justify-center items-center">
+        <div v-if="quote" class="p-4 rounded-lg shadow-xl border border-gray-300 bg-white flex flex-col gap-2">
+            <p>{{ quote.quote }}</p>
+            <p className="text-sm self-end text-gray-500">{{ quote.author }}</p>
+        </div>
+        <div v-else-if="loading">Loading...</div>
+    </div>
 </template>
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+type Quote = { quote: string; author: string }
 
-<style scoped>
-</style>
+const quote = ref<Quote>()
+const loading = ref(false)
+onMounted(async () => {
+    loading.value = true
+    try {
+        await delay(5_000)
+        const r = await fetch('https://dummyjson.com/quote/392')
+        quote.value = await r.json() as Quote
+    } finally {
+        loading.value = false
+    }
+})
+function delay(ms: number) { return new Promise(resolve => setTimeout(resolve, ms)) }
+</script>
