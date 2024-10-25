e95d3909e87f8048b9999960c1d4b38f24beed50
stepan.rutz <stepan.rutz@gmx.de>
2024-10-25 17:29:43 +0200

    more

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,25 @@
 <script setup lang="ts">
+import { provide, ref } from 'vue';
+import Child from './Child.vue'
+import GrandChild from './GrandChild.vue'
+
+const globalState = ref({
+  user: 'Max',
+  age: 30
+})
+
+export type GlobalStateType = typeof globalState
+
+provide('globalState', globalState)
+
+
 </script>
 
 <template>
-  <div class="text-3xl">hi</div>
+    <div>Hello World</div>
+    <Child>
+        <GrandChild></GrandChild>
+    </Child>
+    <button @click="globalState.age++">Change Age</button>
 </template>
 
-<style scoped>
-</style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { createPinia } from 'pinia'
 
-createApp(App).mount('#app')
+const pinia = createPinia()
+
+createApp(App).use(pinia).mount('#app')
+
