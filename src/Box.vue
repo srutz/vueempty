d50c741608404ff53e29d224911d324565351ed1
stepan.rutz <stepan.rutz@gmx.de>
2024-10-29 13:08:20 +0100

    finally moved box into separate components

---
+++ src/Box.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="flex flex-col gap-2 shadow-xl rounded-lg m-8 bg-white p-4" v-auto-animate>
+        <slot></slot>
+    </div>
+</template>
+
--- src/HeaderPanel.vue
+++ src/HeaderPanel.vue
@@ -1,25 +1,23 @@
 <template>
-    <div class="flex flex-col gap-2 shadow-xl rounded-lg m-8 bg-white p-4" v-auto-animate>
-        <div class="flex gap-2">
-            <SpinButton @toggle="onToggle"></SpinButton>
-            <button v-if="false" @click="open = !open"> {{ icon }}</button>
-            <h1 class="text-2xl">{{ header }}</h1>
-        </div>
-        <div v-if="open" >
-            <slot></slot>
-        </div>
+    <div class="flex flex-col">
+        <Box >
+            <div class="flex gap-2">
+                <SpinButton @toggle="onToggle"></SpinButton>
+                <h1 class="text-2xl">{{ header }}</h1>
+            </div>
+            <div v-if="open" >
+                <slot></slot>
+            </div>
+        </Box>
     </div>
 </template>
 <script setup lang="ts">
 import SpinButton from "./SpinButton.vue"
-import { computed, ref } from 'vue';
+import Box from "./Box.vue"
+import { ref } from 'vue';
 
 const { header} = defineProps<{header: string}>()
 const open = ref(true)
-const icon = computed(() => {
-    return open.value ? "▼" : "▶"
-})
-
 
 const onToggle = () => open.value = !open.value
 
