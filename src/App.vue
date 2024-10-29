30f12fb07d92ab60aa3d8a0648bca2266469db34
stepan.rutz <stepan.rutz@gmx.de>
2024-10-29 14:31:25 +0100

    loading state

--- src/App.vue
+++ src/App.vue
@@ -9,22 +9,31 @@ function formatGermanNumber(n: number) {
     })
     return nf.format(n)
 }
+async function delay(millis: number) {
+    return new Promise(resolve => setTimeout(resolve, millis))
+}
 
 const PAGE_SIZE = 3
 
 /* 1: zustand, page und aktuelle products */
 const page = ref(1)
 const products = ref<Product[]>([])
+const loading = ref(false)
 
 
 /* 2: Funktion um Produkte der Page N in den zustand zu laden */
 const loadProducts = async () => {
-
-    const res = await fetch("https://dummyjson.com/products"
-        + "?skip=" + ((page.value - 1) * PAGE_SIZE)
-        + "&limit=" + PAGE_SIZE)
-    const data = await res.json() as ProductsResponse
-    products.value = [... products.value, ...data.products]
+    loading.value = true
+    try {
+        const res = await fetch("https://dummyjson.com/products"
+            + "?skip=" + ((page.value - 1) * PAGE_SIZE)
+            + "&limit=" + PAGE_SIZE)
+        const data = await res.json() as ProductsResponse
+        await delay(3_000)
+        products.value = [... products.value, ...data.products]
+    } finally {
+        loading.value = false
+    }
 }
 
 /* 3: Initiales laden */
@@ -39,8 +48,9 @@ watch([page], () => loadProducts())
 
 <template>
   <div class="h-1 grow flex flex-col gap-2">
-    <button @click="page++">Load more {{ page }}</button>
-    <div class="h-1 grow overflow-scroll flex flex-col gap-2">
+    <button :disabled="loading" @click="page++">Load more {{ page }}</button>
+    <div v-if="loading">Loading ...</div>
+    <div v-else class="h-1 grow overflow-scroll flex flex-col gap-2">
         <div v-for="p in products">
             {{ p.title }} {{ formatGermanNumber(p.price)}}        
         </div>
