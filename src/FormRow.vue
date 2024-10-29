944253a99f5e4a89685754e774fa74de12e2817e
stepan.rutz <stepan.rutz@gmx.de>
2024-10-29 07:49:15 +0100

    added formrow

---
+++ src/FormRow.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="flex gap-2">
+        <label class="w-32" :style="style" :for="labelfor" @click="onLabelClick">{{ label }}</label>
+        <slot></slot>
+    </div>
+</template>
+<script lang="ts" setup>
+import { computed, CSSProperties, ref } from 'vue';
+
+
+type FormRowProps = {
+    label?: string
+    labelfor: string
+    bold?: boolean
+}
+let { label, labelfor, bold } = defineProps<FormRowProps>()
+
+const fontSize = ref(16)
+
+const style = computed(() => {
+    return {
+        fontWeight: bold ? "900" : undefined,
+        fontSize: fontSize.value + "px",
+    } satisfies CSSProperties
+})
+
+const onLabelClick = () => {
+    fontSize.value += 4
+}
+
+
+</script>
--- src/UserForm.vue
+++ src/UserForm.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 
+import FormRow from "./FormRow.vue"
 import { reactive } from "vue"
 
 const formdata = reactive({
@@ -11,16 +12,10 @@ const formdata = reactive({
 </script>
 
 <template>
-    <form class="flex flex-col gap-2 border-solid p-4 border-2 border-red-200">
-        <div class="flex gap-2">
-            <label class="w-32" for="firstname">Firstname</label><input id="firstname" type="text" v-model="formdata.firstname" />
-        </div>
-        <div class="flex gap-2">
-            <label class="w-32" for="lastname">Lastname</label><input id="lastname" type="text" v-model="formdata.lastname" />
-        </div>
-        <div class="flex gap-2">
-            <label class="w-32" for="email">Email</label><input id="email" type="email" v-model="formdata.email" />
-        </div>
+    <form class="flex flex-col font-sans gap-2 m-2 p-4 border border-gray-300 shadow-xl rounded-lg">
+        <FormRow label="Vorname" labelfor="firstname"><input id="firstname" type="text" v-model="formdata.firstname" /></FormRow>
+        <FormRow label="Nachname" labelfor="lastname"><input id="lastname" type="text" v-model="formdata.lastname" /></FormRow>
+        <FormRow bold label="Email" labelfor="email"><input id="email" type="email" v-model="formdata.email" /></FormRow>
         <div>Welcome {{ formdata.firstname }} {{  formdata.lastname }}</div>
     </form>
     
