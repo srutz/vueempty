d03cec4376a51e885af5c4d5bc11019fba4a14ce
stepan.rutz <stepan.rutz@gmx.de>
2024-10-29 11:10:52 +0100

    headerpanel

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,7 @@
 <script setup lang="ts">
 import HeaderPanel from "./HeaderPanel.vue"
 
-// ▶▼ <-- my icons for later
-
-
 </script>
-
-
 <template>
     <HeaderPanel header="Hello World">
         <div>Lorem ipsum lorem dings</div>
--- src/HeaderPanel.vue
+++ src/HeaderPanel.vue
@@ -1,11 +1,21 @@
 <template>
     <div class="flex flex-col gap-2 shadow-xl rounded-lg m-8 bg-white p-4">
-        <h1 class="text-2xl">{{ header }}</h1>
-        <slot></slot>
+        <div class="flex gap-2">
+            <button @click="open = !open"> {{ icon }}</button>
+            <h1 class="text-2xl">{{ header }}</h1>
+        </div>
+        <template v-if="open">
+            <slot></slot>
+        </template>
     </div>
 </template>
 <script setup lang="ts">
+import { computed, ref } from 'vue';
 
 const { header} = defineProps<{header: string}>()
+const open = ref(true)
+const icon = computed(() => {
+    return open.value ? "▼" : "▶"
+})
 
 </script>
