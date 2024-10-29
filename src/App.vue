fd125f6c03916b26671437657bb78e5f142ed1a0
stepan.rutz <stepan.rutz@gmx.de>
2024-10-29 14:07:55 +0100

    loading data from dummyjson

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,34 @@
 <script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import { Product, ProductsResponse } from './Types';
+
+function formatGermanNumber(n: number) {
+    const nf = new Intl.NumberFormat("de-DE", {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+    })
+    return nf.format(n)
+}
+
+const products = ref<Product[]>([])
+const page = ref(1)
+
+const PAGE_SIZE = 20
+
+onMounted(async () => {
+    const res = await fetch("https://dummyjson.com/products?" + (page.value - 1 * PAGE_SIZE))
+    const data = await res.json() as ProductsResponse
+    products.value = data.products
+})
+
 </script>
 
 <template>
-  <div class="text-3xl">hi</div>
+  <div class="flex flex-col gap-2">
+    <div v-for="p in products">
+        {{ p.title }} {{ formatGermanNumber(p.price)}}        
+    </div>
+  </div>
 </template>
 
 <style scoped>
