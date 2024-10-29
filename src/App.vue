b8981fe9f6277851b8232c865def39bdb1762033
stepan.rutz <stepan.rutz@gmx.de>
2024-10-29 09:46:39 +0100

    windowsize listening

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,29 @@
 <script setup lang="ts">
+import { onMounted, onUnmounted, ref } from 'vue';
+
+const width = ref(window.innerWidth)
+const height = ref(window.innerHeight)
+
+// variante 1
+const listener = () => {
+    width.value = window.innerWidth
+    height.value = window.innerHeight
+}
+
+onMounted(() => {
+    window.addEventListener("resize", listener)
+})
+
+onUnmounted(() => {
+    window.removeEventListener("resize", listener)
+})
+
 </script>
 
 <template>
-  <div class="text-3xl">hi</div>
+    <div class="m-4 text-3xl">
+        Size: {{ width }} x {{ height }}
+    </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
