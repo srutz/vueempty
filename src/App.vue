3230074ed873237e1e261b6fe46392a727fc459e
stepan.rutz <stepan.rutz@gmx.de>
2025-02-01 16:37:48 +0100

    vscode

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,24 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+import { useQuotes } from './useQuotes';
+
+
+const page = ref(1)
+const { quotes, loading } = useQuotes(page)
+
 </script>
 
 <template>
-  <div>hi</div>
+    <div class="grow bg-orange-500">
+        <div v-if="loading" class="text-center">
+            <p>Loading...</p>
+        </div>
+        <div v-else>
+            <div v-for="(quote, i) in quotes" :key="quote.id">
+                <p class="motion-preset-slide-right" >{{ quote.quote }}</p>
+            </div>
+        </div>
+    </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
