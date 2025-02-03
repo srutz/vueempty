8a4c6cf1ca3010500f71869c7a549f5269b15d81
Stepan Rutz <stepan.rutz@gmx.de>
2025-02-03 14:11:01 +0100

    box

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
-
+import Box from "./Box.vue" 
 </script>
 
 <template>
-    <div class="grow bg-pink-500">
-        Hello World
+    <div class="grow bg-gray-300">
+        <Box title="abcdef" footer="Köln, 2025">Hello World</Box>
     </div>
 </template>
 
-<style scoped></style>
---
+++ src/Box.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="shadow-xl rounded-lg bg-white p-4 m-4
+        motion-preset-slide-right flex flex-col">
+        <div class="font-bold">{{ props.title }}</div>
+        <div><slot></slot></div>
+        <div v-if="props.footer" class="text-gray-500 text-sm self-end">
+            {{ props.footer }}</div>
+    </div>
+</template>
+<script lang="ts" setup>
+type PropsType = { 
+    title: string 
+    footer?: string
+}
+const props = defineProps<PropsType>()
+</script>
