a3bed430bfb199decd969d7b56624ecee5ff6001
Stepan Rutz <stepan.rutz@gmx.de>
2025-02-03 14:57:47 +0100

    moved button into box

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,10 @@
 <script setup lang="ts">
-import { ref } from "vue";
 import Box from "./Box.vue" 
-const open = ref(false)
 </script>
 
 <template>
     <div class="grow bg-gray-300">
-        <Box title="abcdef" :expanded="open" @click="open = !open">
+        <Box title="abcdef" expanded >
             Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
         </Box>
     </div>
--- src/Box.vue
+++ src/Box.vue
@@ -1,21 +1,25 @@
 <template>
-    <div class="shadow-xl rounded-lg bg-white p-4 m-4
+    <div class="shadow-xl rounded-lg bg-white p-4 m-4 gap-2
         motion-preset-slide-right flex flex-col" v-auto-animate>
-        <div class="font-bold cursor-pointer select-none">
+        <div class="font-bold cursor-pointer text-lg select-none" @click="open = !open">
             {{ expanded ? "▾" : "▸" }}
             {{ title }}
         </div>
-        <div v-if="expanded" ><slot></slot></div>
+        <div v-if="open" ><slot></slot></div>
         <div v-if="footer" class="text-gray-500 text-sm self-end">
             {{ footer }}</div>
     </div>
 </template>
+
 <script lang="ts" setup>
 import { vAutoAnimate } from '@formkit/auto-animate';
+import { ref } from 'vue';
 type PropsType = { 
     title: string 
     footer?: string
     expanded?: boolean
 }
-const { title, expanded, footer } = defineProps<PropsType>() 
+
+let { title, expanded, footer = "Deutzer-Greetings"} = defineProps<PropsType>() 
+const open = ref(expanded)
 </script>
