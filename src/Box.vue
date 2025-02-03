5918cfc8b75f47d5991ac48e6cac9c12271acbcb
Stepan Rutz <stepan.rutz@gmx.de>
2025-02-03 14:36:25 +0100

    box

--- src/Box.vue
+++ src/Box.vue
@@ -1,16 +1,21 @@
 <template>
     <div class="shadow-xl rounded-lg bg-white p-4 m-4
-        motion-preset-slide-right flex flex-col">
-        <div class="font-bold">{{ props.title }}</div>
-        <div><slot></slot></div>
-        <div v-if="props.footer" class="text-gray-500 text-sm self-end">
-            {{ props.footer }}</div>
+        motion-preset-slide-right flex flex-col" v-auto-animate>
+        <div class="font-bold cursor-pointer select-none">
+            {{ expanded ? "▾" : "▸" }}
+            {{ title }}
+        </div>
+        <div v-if="expanded" ><slot></slot></div>
+        <div v-if="footer" class="text-gray-500 text-sm self-end">
+            {{ footer }}</div>
     </div>
 </template>
 <script lang="ts" setup>
+import { vAutoAnimate } from '@formkit/auto-animate';
 type PropsType = { 
     title: string 
     footer?: string
+    expanded?: boolean
 }
-const props = defineProps<PropsType>()
+const { title, expanded, footer } = defineProps<PropsType>() 
 </script>
