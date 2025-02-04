1098ee5149b96cd4516492ea4a6f0dd9db13faa5
stepan.rutz <stepan.rutz@gmx.de>
2025-02-04 13:22:48 +0100

    More

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <script setup lang="ts">
-import { computed, ref } from "vue"
 import Box from "./Box.vue"
 import Product from "./Product.vue"
-import H1 from "./H1.vue"
-import VCol from "./VCol.vue"
-import VRow from "./VRow.vue"
 import { useProduct } from "./useProduct"
 
 const { product } = useProduct(20)
@@ -14,18 +10,16 @@ const format = new Intl.NumberFormat("de-DE", {
 })
 const f = (n?: number) => n ? format.format(n) : "-"
 
-const boxOpen = ref(true)
+// event handler für custom-emit "expanded" der Box
 const handleExpanded = (open: boolean, n: number) => {
     console.log('expanded changed', open, n)
-    boxOpen.value = open
 }
 
 </script>
 
 <template>
     <div class="grow bg-gray-300 flex flex-col gap-4"  >
-        <H1>Überschrift 123</H1>
-        <Box :title='"Produkt " + product?.id' expanded
+        <Box :title='"Produkt " + (product?.id || "")' expanded
                 @expanded="handleExpanded">
             <template #content>
                 <Product :product="product" />
--- src/useWindowSize.ts
+++ src/useWindowSize.ts
@@ -1,13 +1,13 @@
-import { onMounted, onUnmounted, ref } from "vue"
+import { onMounted, onUnmounted, reactive, ref } from "vue"
 
 export function useWindowSize() {
-    const size = ref({ width: window.innerWidth, height: window.innerHeight })
+    const size = reactive({ width: window.innerWidth, height: window.innerHeight })
     onMounted(() => {
         const listener = () => {
-            console.log(window.innerWidth, window.innerHeight)
-            size.value = { width: window.innerWidth, height: window.innerHeight }
-            //size.value.width = window.innerWidth
-            //size.value.height = window.innerHeight
+            //console.log(window.innerWidth, window.innerHeight)
+            //size.value = { width: window.innerWidth, height: window.innerHeight }
+            size.width = window.innerWidth
+            size.height = window.innerHeight
         }
         window.addEventListener("resize", listener)
         onUnmounted(() => {
